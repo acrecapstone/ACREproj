bfc76c98d890c0af0a7596e3223d353ca39333af
MacMini1 <macmini1@macmini-120d86.eil-server.cba.ua.edu>
2015-10-19 14:03:19 -0500

    Initial Commit 10/19

---
+++ HomePageViewController.swift
@@ -0,0 +1,67 @@
+//
+//  HomePageViewController.swift
+//  ACRE_App
+//
+//  Created by Tulsi Patel on 10/8/15.
+//  Copyright © 2015 AcreApp.Practice. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class HomePageViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource{
+    //variables
+    @IBOutlet weak var mlsCityPickerView: UIPickerView!
+    @IBOutlet weak var timeReportPickerView: UIPickerView!
+    
+    var pickerDataSource = ["140-Trussville", "139-Hoover", "138-Birmingham"]
+    var timePickerView = ["Monthly", "Quarterly"]
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        //if (mlsCityPickerView.tag == 12)
+        //{
+        return pickerDataSource.count;
+        //}
+        //else
+        //{
+            //return timePickerView.count;
+        //}
+    }
+
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        //if (mlsCityPickerView.tag == 12)
+        //{
+        return pickerDataSource[row]
+        //}
+        //else
+        //{ return timePickerView[row]}
+    }
+
+    func numberOfComponentsInTimeReportPickerView(pickerView: UIPickerView) -> Int { return 1 }
+    
+    func timeReportPickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return timePickerView.count;
+    }
+
+    func timeReportPickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return timePickerView[row]
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.mlsCityPickerView.dataSource = self;
+        self.mlsCityPickerView.delegate = self;
+        self.timeReportPickerView.dataSource = self;
+        self.timeReportPickerView.delegate = self;
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
---
+++ RequestAuthenticationDataProvider.swift
@@ -0,0 +1,31 @@
+//
+//  RequestAuthenticationDataProvider.swift
+//  ACRE_App
+//
+//  Created by MacMini1 on 10/19/15.
+//  Copyright © 2015 AcreApp.Practice. All rights reserved.
+//
+
+/**import Foundation
+import UIKit
+
+class RequestAuthenticationDataProvider {
+
+    var reqAuthToken = NSURLSessionDataTask()
+    var session: NSURLSession{
+        return NSURLSession.sharedSession()
+    }
+    
+    let urlString = "http://localhost:9207/api/user/1?email=cbboyd2@gmail.com"
+    
+    let url = NSURL (string: urlString)
+    var requestAuthentication: Bool = false
+    var loggedIn: String = "Failed"
+    
+
+
+
+
+
+
+}**/
