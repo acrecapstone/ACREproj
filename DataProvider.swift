5c3513b1e1314c148bc72f88e5f116a658fb3e7c
MacMini1 <macmini1@macmini-120d86.eil-server.cba.ua.edu>
2015-10-20 16:36:33 -0500

    Test

--- DataProvider.swift
+++ DataProvider.swift
@@ -17,15 +17,21 @@ class DataProvider
     
     func requestAuthentication ()
     {
-        var url: String = "http://localhost:9207/api/user/1?email=cbboyd2@gmail.com"
-        var request: NSMutableURLRequest = NSMutableURLRequest()
-        request.URL = NSURL(string: url)
-        request.HTTPMethod = "GET"
+        let urlPath : String = "http://localhost:9207/api/user/1?email=cbboyd2@gmail.com"
+        let url: NSURL = NSURL(string: urlPath)!
+        let request: NSMutableURLRequest = NSMutableURLRequest(URL: url)
+        var response: AutoreleasingUnsafeMutablePointer<NSURLResponse?>=nil
+        var dataVal: NSData = NSURLConnection.sendSynchronousRequest(request, returningResponse: response, error: nil)
+        var err: NSError
+        print(response)
+        var jsonResult: NSDictionary = (NSJSONSerialization.JSONObjectWithData(dataVal, options: NSJSONReadingOptions.MutableContainers, error:&err) as? NSDictionary)!
+        print(Synchronous\(jsonResult))
         
-        NSURLConnection.sendSynchronousRequest(request, returningResponse: &response, error: nil)
-            
-    
-    
+
+        //request.URL = NSURL(string: url)
+        //request.HTTPMethod = "GET"
+        
+        
     
     }
 
@@ -34,4 +40,5 @@ class DataProvider
 
 
 }
+
 
